a1ec574b4027d34cfd0f05ca0b0750f269dd96b2
renansep <renan@Renans-MacBook-Pro-2.local>
2014-03-10 11:33:36 -0300

    Initial Commit

---
+++ MatchX/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  MatchX
+//
+//  Created by Renan Cargnin on 3/10/14.
+//  Copyright Renan Cargnin 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
